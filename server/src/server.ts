69a30762987150abc76772ec78857f9db17def47
Breno Ramalho Lemes <breno@br-lemes.net>
2023-05-19 22:24:11 -0400

    Prisma and users table database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
+import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get('/hello', () => 'Hello World!');
+app.get('/users', async () => {
+    return await prisma.user.findMany();
+});
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server listening on port 3333');
