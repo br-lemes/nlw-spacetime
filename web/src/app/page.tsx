da2f1fa3f67326fd63c51cfbb003242da507b74c
Breno Ramalho Lemes <breno@br-lemes.net>
2023-05-19 23:45:22 -0400

    Next Hello World

---
+++ web/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+const Home = () => (
+    <div className='bg-zinc-800 flex h-screen items-center justify-center'>
+        <h1 className='font-bold text-2xl text-zinc-200'>Hello World!</h1>
+    </div>
+);
+
+export default Home;
